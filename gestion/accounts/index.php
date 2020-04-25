b753b0e922f191e0bc498caa73de73fdfa743018
Ovvvs <fantomix4@gmail.com>
2020-04-25 14:36:58 +0200

    Broutille Changement d'écriture

--- gestion/accounts/index.php
+++ gestion/accounts/index.php
@@ -10,7 +10,7 @@
  </head>
  <body>
 	 <p id="gestionNav">
-		 <a href="../">Espace de gestion</a> >
+		 <a href="../">Espace administration du panneau</a> >
 		 <a href="?">Gestion des comptes</a>
 	 </p>
 	 <div id="espaceErreur">

e926500d43244449ac6ccc44c84ea3c94cae02ba
Ovvvs <fantomix4@gmail.com>
2020-05-07 17:54:43 +0200

    Frontend Création de la page pour le design, normalement design avec les mesures respectées

---
+++ panneau.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>L'affichage du panneau</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="jour">
+      jour
+    </div>
+    <div class="contenu_news">
+      contenu de la news
+    </div>
+    <div class="heure">
+      heure
+    </div>
+    <div class="titre_news_1">
+      news 1
+    </div>
+    <div class="titre_news_2">
+      news 2
+    </div>
+    <div class="titre_news_3">
+      news 3
+    </div>
+    <div class="titre_news_4">
+      news 4
+    </div>
+  </body>
+</html>
